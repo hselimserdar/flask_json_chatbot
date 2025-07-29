89cab4f2dfa8955d78bcea81e87bf5054850ca70
H. Selim SERDAR <selimserdar2002@gmail.com>
2025-07-29 13:42:03 +0300

    bugfix

--- __init__.py
+++ __init__.py
@@ -9,41 +9,7 @@ signed_user = "guest"
 
 if debugging:
     print("Debugging is enabled.")
-
-def search_for_existing_user(username): #returns password if user exists
-    database = sqlite3.connect('database.sqlite')
-    db_cursor = database.cursor()
-    try:
-        db_cursor.execute("SELECT username, password FROM user")
-        for user in db_cursor.fetchall():
-            if user[0] == username:
-                if debugging:
-                    print("User found: ", user)
-                    return user[1]
-    except sqlite3.Error as e:
-        print("SQLite error occurred:", e)
-        return None
-    finally:
-        db_cursor.close()
-        database.close()
-    return None
-
-def add_new_user(username, password):
-    database = sqlite3.connect('database.sqlite')
-    db_cursor = database.cursor()
-    
-    try:
-        temp = "INSERT INTO user (username, password) VALUES ('" + str(username) + "', '" + str(password) + "')"
-        print (temp)
-        db_cursor.execute(temp)
-        database.commit()
-        return True
-    except sqlite3.Error as e:
-        print("SQLite error occurred:", e)
-        return False
-    finally:
-        db_cursor.close()
-        database.close()
+    print("Current signed user: ", signed_user)
 
 @app.route('/')
 def landing_page():
